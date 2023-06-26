e9a8ca7dc94160d7013b09f702cb70e2b0f018bf
Kevin Castillo <kevinacastles@gmail.com>
2023-06-26 19:19:51 -0400

    feat: add cursor glow effect

---
+++ src/components/Cursor.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Cursor = () => {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [hidden, setHidden] = useState(false);
+    useEffect(() => {
+        const addEventListeners = () => {
+            document.addEventListener('mousemove', mMove);
+            document.addEventListener('mouseenter', mEnter);
+            document.addEventListener('mouseleave', mLeave);
+        };
+
+        const removeEventListeners = () => {
+            document.removeEventListener('mousemove', mMove);
+            document.removeEventListener('mouseenter', mEnter);
+            document.removeEventListener('mouseleave', mLeave);
+
+        };
+
+        const mMove = (el) => {
+            setPosition({ x: el.clientX, y: el.clientY });
+        };
+
+        const mLeave = () => {
+            setHidden(true);
+        };
+
+        const mEnter = () => {
+            setHidden(false);
+        };
+
+        addEventListeners();
+        return () => removeEventListeners();
+    }, []);
+
+
+    return (
+        <div
+            className={`fixed w-0 h-0 rounded-full shadow-[0_0_60px_40px] shadow-secondary-700 dark:shadow-secondary-500 translate-x-[-50%] translate-y-[-50%] ${hidden ? 'c--hidden ' : ' '}`}
+            style={{
+                left: `${position.x}px`,
+                top: `${position.y}px`,
+            }}
+        />
+    );
+};
+
+export default Cursor;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,12 +4,13 @@ import '@/styles/globals.css'
 import { Poppins } from "next/font/google"
 import Head from 'next/head'
 import { ThemeProvider } from "next-themes"
+import Cursor from '@/components/Cursor'
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   style: ['normal', 'italic'],
-})
+});
 
 function Application ({ Component, pageProps }) {
   return (
@@ -20,6 +21,7 @@ function Application ({ Component, pageProps }) {
 
       </Head>
       <main className={poppins.className}>
+        <Cursor />
         <ThemeProvider attribute="class">
           <NavBar />
           <Component {...pageProps} />
