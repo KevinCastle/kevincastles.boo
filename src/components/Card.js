0d865e230f34a92193331f87ffa5d22bbac7d7d1
Kevin Castillo <kevinacastles@gmail.com>
2023-06-26 17:03:14 -0400

    style: increment text size and add text align

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import {customLink as Link} from './Link';
 import React from 'react'
 
 function CardPlaceholder ({ placeholder }) {
-    return placeholder ? <p className='text-gray-600 dark:text-gray-500'>{placeholder}:</p> : null;
+    return placeholder ? <p className='text-lg text-gray-600 dark:text-gray-500'>{placeholder}:</p> : null;
 }
 
 function CardLabel ({ label }) {
-    return label ? <p className='text-lg font-bold'>{label}</p> : null;
+    return label ? <p className='text-xl font-bold text-right'>{label}</p> : null;
 }
 
 function CardLink ({ children, link }) {
@@ -17,14 +17,16 @@ function CardLink ({ children, link }) {
 
 function Card ({ children, className = "", link, linkText, label, placeholder }) {
     return (
-        <div className={`bg-lightless dark:bg-darkless p-5 rounded-2xl drop-shadow-md hover: ${className} hover:-translate-y-1 transition duration-300`}>
+        <div className={`bg-lightless dark:bg-darkless p-5 rounded-2xl drop-shadow-md hover:-translate-y-1 transition duration-300 ${className}`}>
             { (placeholder || label || link) && <div className="flex justify-between items-center h-full">
                 <CardPlaceholder placeholder={placeholder} />
                 <CardLabel label={label} />
                 <CardLink link={link}>{linkText}</CardLink>
             </div> }
-            <div>
-                {children}
+            <div className='h-full w-full flex items-center justify-center'>
+                <div className='w-full'>
+                    {children}
+                </div>
             </div>
         </div>
     )
