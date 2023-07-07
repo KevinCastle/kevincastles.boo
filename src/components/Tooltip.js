937f0957e795d8b1c6b0b9da7d123a819d26d7c3
Kevin Castillo <kevinacastles@gmail.com>
2023-07-07 16:34:53 -0400

    feat: set React format

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Tooltip = ({ message, children }) => {
     return (
-        <div class="group relative flex">
+        <div className="group relative flex">
             {children}
-            <span class="absolute top-10 left-[50%] translate-x-[-50%] scale-0 transition-all rounded bg-secondary-800 p-2 text-xs text-light group-hover:scale-100">{message}</span>
+            <span className="absolute top-10 left-[50%] translate-x-[-50%] scale-0 transition-all rounded bg-secondary-800 p-2 text-xs text-light group-hover:scale-100">{message}</span>
         </div>
     )
 }
