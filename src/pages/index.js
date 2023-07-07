a40df40184e5e412db1b68db9a68e1a2aa60c8cd
Kevin Castillo <kevinacastles@gmail.com>
2023-07-07 16:32:38 -0400

    feat: add background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,14 @@ export default function Home () {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Kevin Castillo&apos;s Blog</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Portfolio and blog" />
       </Head>
 
-      <main className='w-full'>
+      <main className='bg-circles w-full h-full relative'>
         <Hero />
         <About />
       </main>
