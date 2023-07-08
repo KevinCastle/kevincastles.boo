c846ec5f62fec2c418ba128e1158acfdb8e209ec
Kevin Castillo <kevinacastles@gmail.com>
2023-07-07 23:16:11 -0400

    fix: delete card unused props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,8 @@
 import {customLink as Link} from './Link';
 
-function CardPlaceholder ({ placeholder }) {
-    return placeholder ? <p className='text-lg text-gray-600 dark:text-gray-500'>{placeholder}:</p> : null;
-}
-
-function CardLabel ({ label }) {
-    return label ? <p className='text-xl font-bold text-right'>{label}</p> : null;
-}
-
-function CardLink ({ children, link }) {
-    return link ? 
-    <Link href={link} className='text-lg font-semibold'>{children}</Link>
-    : null;
-}
-
-function Card ({ children, className = "", link, linkText, label, placeholder }) {
+function Card ({ children, className = "" }) {
     return (
         <div className={`bg-lightless/30 dark:bg-darkless/50 border border-solid border-light/[0.18] dark:border-dark/[0.18] backdrop-blur-sm p-5 rounded-2xl drop-shadow-md hover:-translate-y-1 transition duration-300 ${className}`}>
-            { (placeholder || label || link) && <div className="flex justify-between items-center h-full">
-                <CardPlaceholder placeholder={placeholder} />
-                <CardLabel label={label} />
-                <CardLink link={link}>{linkText}</CardLink>
-            </div> }
             <div className='h-full w-full flex items-center justify-center'>
                 <div className='w-full'>
                     {children}
