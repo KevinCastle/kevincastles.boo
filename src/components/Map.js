43e12adfbc35cd7bfef4a36ad1ad08a44d4f6fcb
Kevin Castillo <kevinacastles@gmail.com>
2023-07-21 03:39:40 -0400

    feat: add map icon pointer

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useEffect, useRef } from 'react';
-import profilePic from '../../public/images/profile/avatar.png'
 
-const Map = () => {
+const Map = ({ className = "" }) => {
     const mapContainer = useRef(null);
     const map = useRef(null);
 
@@ -28,36 +27,32 @@ const Map = () => {
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [-70.64827, -33.45694], // center map on Chad
-            zoom: 10
+            center: [-70.64827, -33.45694],
+            zoom: 11
         });
         
-        const marker = geojson.features[0]; // Get the first marker
-        
-        // // Create a DOM element for the marker
-        // const markerIcon = document.createElement('div');
-        // markerIcon.className = 'markerIcon w-[30%] h-[30%] bg-[length:100%] bg-no-repeat';
-        // markerIcon.style.backgroundImage = `url('/images/profile/avatar.png')`;
+        const marker = geojson.features[0];
 
         const markerIcon = document.createElement('div');
-        markerIcon.className = 'markerIcon w-[65px] h-[65px] hover:w-[85px] hover:h-[85px] bg-primary-500/20 border-2 hover:border-4 border-solid border-white rounded-full overflow-hidden flex justify-center items-center hover:transition-all ease-in-out duration-500';
+        markerIcon.className = 'markerIcon w-[85px] h-[85px] hover:w-[105px] hover:h-[105px] bg-primary-500/20 border-4 hover:border-6 border-solid border-white rounded-full overflow-hidden flex justify-center items-center hover:transition-all ease-in-out duration-500';
 
-        // Create the image element
         const image = document.createElement('img');
         image.src = '/images/profile/avatar.png';
-        image.className = 'w-[80%] h-[80%] rounded-full';
+        image.className = 'w-[100%] h-[100%] object-cover';
 
-        // Append the image to the container
         markerIcon.appendChild(image);
 
         new mapboxgl.Marker(markerIcon)
         .setLngLat(marker.geometry.coordinates)
         .addTo(map.current);
+
+        const mapboxInfo = document.querySelector('.mapboxgl-control-container');
+        mapboxInfo.remove();
     }, []);
         
   return (
       <div className='w-full h-full'>
-          <div className="map-container" ref={mapContainer} style={{ width: '100%', height: '100%' }} />
+          <div className={className} ref={mapContainer} />
     </div>
   )
 }
