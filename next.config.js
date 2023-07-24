98e010b85701244f7373312f1831db13d319cdbd
Kevin Castillo <kevinacastles@gmail.com>
2023-07-23 23:59:15 -0400

    feat: add spotify api connect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     env: {
         NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
     },
+    images: {
+        domains: ['i.scdn.co'],
+    },
 }
 
 module.exports = nextConfig
