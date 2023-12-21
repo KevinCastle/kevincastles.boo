6ed1f62c47d46da6d8b3c13a917adba19b4099de
Kevin Castillo <kevinacastles@gmail.com>
2023-12-21 00:22:20 -0300

    fix: unnecesary env set

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
-  },
   images: {
     domains: ['i.scdn.co'],
   },
